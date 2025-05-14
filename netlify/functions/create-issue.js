edd85d7e4d4e410eb960811346519261855f8941
Arild <142302189+ArildR82@users.noreply.github.com>
2025-05-14 13:47:01 +0200

    Add files via upload

---
+++ netlify/functions/create-issue.js
@@ -0,0 +1,38 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+exports.handler = async (event, context) => {
+  const { title, body } = JSON.parse(event.body);
+
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const OWNER = 'ArildR82'; // <-- Bytt ut
+  const REPO = 'https://github.com/PorticoEstate/PorticoEstate';         // <-- Bytt ut
+
+  const url = `https://api.github.com/repos/${OWNER}/${REPO}/issues`;
+
+  const githubResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${GITHUB_TOKEN}`,
+      'Accept': 'application/vnd.github+json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: title,
+      body: body
+    })
+  });
+
+  if (!githubResponse.ok) {
+    const errorData = await githubResponse.json();
+    return {
+      statusCode: githubResponse.status,
+      body: JSON.stringify({ message: `Error: ${errorData.message}` })
+    };
+  }
+
+  const data = await githubResponse.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Issue opprettet! Nummer: ${data.number}` })
+  };
+};
