49a7475cd884627fe54ec05c82cc3b7536516d58
Arild <142302189+ArildR82@users.noreply.github.com>
2025-05-14 13:54:01 +0200

    Update create-issue.js

--- netlify/functions/create-issue.js
+++ netlify/functions/create-issue.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
 
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
   const OWNER = 'ArildR82'; // <-- Bytt ut
-  const REPO = 'https://github.com/PorticoEstate/PorticoEstate';         // <-- Bytt ut
+  const REPO = 'aktiv-kommune-skjema';         // <-- Bytt ut
 
   const url = `https://api.github.com/repos/${OWNER}/${REPO}/issues`;
 
